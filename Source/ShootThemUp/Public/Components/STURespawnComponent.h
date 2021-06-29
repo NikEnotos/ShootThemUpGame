25eff464c75c5181309b10b4e5460df12e3aa29e
NikEnotos <supper.nik2002@gmail.com>
2021-06-29 16:41:58 +0300

    Added count down widget on death

--- Source/ShootThemUp/Public/Components/STURespawnComponent.h
+++ Source/ShootThemUp/Public/Components/STURespawnComponent.h
@@ -18,6 +18,10 @@ public:
 
 	void Respawn(int32 RespawnTime);
 
+	int32 GetRespawnCountDown() const { return RespawnCountDown; }
+
+	bool IsRespawnInProgress() const;
+
 private:
 
 	FTimerHandle RespawnTimerHandle;
